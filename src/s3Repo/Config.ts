0e39a99c2b88929aefc873d0bcc7e7300713d3e1
Lu Cui <lu.cui@learningpool.com>
2022-05-25 12:52:24 +0100

    refactor: Update s3Repo Config client type (LLC-1825)

--- src/s3Repo/Config.ts
+++ src/s3Repo/Config.ts
@@ -1,6 +1,6 @@
-import { S3 } from '@aws-sdk/client-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 interface Config {
-  readonly client: S3;
+  readonly client: S3Client;
   readonly bucketName: string;
   readonly subFolder: string;
 }
