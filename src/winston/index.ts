c0861faa257c78861c6bf700d25ad93e5a65e9e2
Lydia Ross <lydia.ross@learningpool.com>
2023-06-12 10:06:59 +0100

    fix: Update usages of winston-cloudwatch

--- src/winston/index.ts
+++ src/winston/index.ts
@@ -1,42 +1,39 @@
 import * as moment from 'moment';
 import * as winston from 'winston';
-import * as CloudWatchTransport from 'winston-aws-cloudwatch';
+import * as CloudWatchTransport from 'winston-cloudwatch';
 import Config from './Config';
 
-const getTime = () => {
-  return moment().format('YYYY-MM-DD HH:mm:ss:SSS');
-};
+const getTime = () => moment().format('YYYY-MM-DD HH:mm:ss:SSS');
 
-const createConsoleTransport = (config: Config): winston.TransportInstance => {
-  return new winston.transports.Console({
-    colorize: true,
-    handleExceptions: true,
-    humanReadableUnhandledException: true,
-    level: config.console.level,
-    prettyPrint: true,
-    stderrLevels: ['error'],
-    timestamp: getTime,
-  });
-};
+const createConsoleTransport = (config: Config): winston.transport => new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({
+      format: getTime,
+    }),
+    winston.format.prettyPrint()
+  ),
+  handleExceptions: true,
+  level: config.console.level,
+  stderrLevels: ['error'],
+});
 
-const createAwsTransport = (config: Config): winston.TransportInstance => {
-  return new CloudWatchTransport({
-    awsConfig: config.cloudWatch.awsConfig,
-    createLogGroup: true,
-    createLogStream: true,
-    level: config.cloudWatch.level,
-    logGroupName: config.cloudWatch.logGroupName,
-    logStreamName: config.cloudWatch.logStreamName,
-  });
-};
+const createAwsTransport = (config: Config): winston.transport => new CloudWatchTransport({
+  ...(config.cloudWatch.awsConfig.accessKeyId && config.cloudWatch.awsConfig.secretAccessKey ? {
+    awsAccessKeyId: config.cloudWatch.awsConfig.accessKeyId,
+    awsSecretKey: config.cloudWatch.awsConfig.secretAccessKey,
+  } : {}),
+  awsRegion: config.cloudWatch.awsConfig.region,
+  logGroupName: config.cloudWatch.logGroupName,
+  logStreamName: config.cloudWatch.logStreamName,
+  level: config.cloudWatch.level,
+});
 
-export default (config: Config) => {
-  winston.cli();
-  return new winston.Logger({
-    exitOnError: false,
-    transports: [
-      createConsoleTransport(config),
-      ...(!config.cloudWatch.enabled ? [] : [createAwsTransport(config)]),
-    ],
-  });
-};
+export default (config: Config) => winston.createLogger({
+  exitOnError: false,
+  format: winston.format.json(),
+  transports: [
+    createConsoleTransport(config),
+    ...(!config.cloudWatch.enabled ? [] : [createAwsTransport(config)]),
+  ],
+});
